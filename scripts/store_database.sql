ed614677ebb0df4318f2a3997362e14d010d4d17
Daniil Pavlov <17dp15@queensu.ca>
2023-09-08 19:05:19 -0400

    Added json result for product list and price table

--- scripts/store_database.sql
+++ scripts/store_database.sql
@@ -17,7 +17,19 @@ create table product(
 	foreign key (store_id) references store(id) on delete cascade
 );
 
+create table price(
+	id int primary key,
+	rate decimal(15, 2),
+	product_id int,
+	foreign key (product_id) references product(id) on delete cascade
+);
+
 INSERT INTO store (id, name, street, city, postal_code) VALUES (1, 'Test Store', 'Test Street', 'Test City', '123123');
+
 INSERT INTO product (id, name, store_id) VALUES (1, 'Test Product 1', 1);
 INSERT INTO product (id, name, store_id) VALUES (2, 'Test Product 2', 1);
 INSERT INTO product (id, name, store_id) VALUES (3, 'Test Product 3', 1);
+
+insert into price (id, rate, product_id) values (1, 2.25, 1);
+insert into price (id, rate, product_id) values (2, 3.75, 2);
+insert into price (id, rate, product_id) values (3, 5.00, 3);
--- src/main/java/com/pavlov/onlinestore/ProductController.java
+++ src/main/java/com/pavlov/onlinestore/ProductController.java
@@ -4,38 +4,45 @@ package com.pavlov.onlinestore;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+@RequestMapping("/product")
 @RestController
 public class ProductController {
 
-    @GetMapping("/product/{param_1}/{param_2}")
+    @GetMapping("/sum/{param_1}/{param_2}")
     public String testSum(@PathVariable Long param_1, @PathVariable Long param_2) {
         return "" + (param_1 + param_2);
     }
 
     @SneakyThrows
-    @GetMapping("/allproducts")
-    public String getAllProducts() {
+    @GetMapping("/all")
+    public List<Product> getAllProducts() {
         Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/online_store?" +
                                                                 "user=root&password=");
         PreparedStatement statement = connection.prepareStatement("select id, name from product");
         ResultSet result_set = statement.executeQuery();
-        String body = "";
+        List<Product> result = new ArrayList<>();
+
         while (result_set.next()) {
-            body += result_set.getInt("id");
-            body += " ";
-            body += result_set.getString("name");
-            body += "\n";
+            Product product = new Product();
+            product.setId(result_set.getInt("id"));
+            product.setName(result_set.getString("name"));
+            result.add(product);
         }
-        return body;
+
+        return result;
+
     }
 
 }
