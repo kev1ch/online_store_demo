d377ae4290aa2d9bff07d5b00b1eca2fc34d2077
Daniil Pavlov <17dp15@queensu.ca>
2024-03-16 13:06:43 -0400

    Added view Cart and cart_line functionality

--- src/main/java/com/pavlov/onlinestore/controllers/CartController.java
+++ src/main/java/com/pavlov/onlinestore/controllers/CartController.java
@@ -1,12 +1,15 @@
 package com.pavlov.onlinestore.controllers;
 
 import com.pavlov.onlinestore.model.CartLine;
+import com.pavlov.onlinestore.model.Product;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("/cart")
@@ -37,12 +40,37 @@ public class CartController {
         // add cart_line into cart
         // insert into cart_line (id, cart_id, product_id, quantity) values (1, 1, 1, 1);
         var connection = dataSource.getConnection();
-        PreparedStatement statement = connection.prepareStatement("insert into cart_line (cart_id, product_id, quantity) values (?, ?, ?)");
+        PreparedStatement statement = connection.prepareStatement("insert into cart_line (cart_id, product_id," +
+                " quantity) values (?, ?, ?)");
         statement.setInt(1, cart_id);
         statement.setInt(2, product_id);
         statement.setInt(3, quantity);
         statement.execute();
         return 0;
+    }
+
+    @SneakyThrows
+    // @CrossOrigin(origins = "*")
+    @GetMapping("/all")
+    public List<CartLine> getAllLines() {
+        PreparedStatement statement = dataSource.getConnection().prepareStatement("select cl.cart_id, p.id, p.name, " +
+                "cl.quantity, pri.rate from cart_line cl, product p, price pri where cart_id = 1 and p.id = cl.product_id " +
+                "and pri.product_id = p.id;");
+        ResultSet result_set = statement.executeQuery();
+        List<CartLine> result = new ArrayList<>();
+
+        while (result_set.next()) {
+            CartLine cart_line = new CartLine();
+            cart_line.setCart_id(1);    //TODO change for multiple cartlines
+            cart_line.setProduct_id(result_set.getInt("id"));
+            cart_line.setProduct_name(result_set.getString("name"));
+            cart_line.setQuantity(result_set.getInt("quantity"));
+            cart_line.setPrice(result_set.getBigDecimal("rate"));
+            result.add(cart_line);
+        }
+
+        return result;
+
     }
 
 }
--- src/main/java/com/pavlov/onlinestore/model/CartLine.java
+++ src/main/java/com/pavlov/onlinestore/model/CartLine.java
@@ -2,8 +2,17 @@ package com.pavlov.onlinestore.model;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class CartLine {
     private int product_id;
     private int quantity;
+    private int cart_id;
+    private String product_name;
+    private BigDecimal price;
+
+
+    // select cl.cart_id, p.id, p.name, cl.quantity, pri.rate from cart_line cl,
+    // product p, price pri where cart_id = 1 and p.id = cl.product_id and pri.product_id = p.id;
 }
