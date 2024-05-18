4dd5d8efa8b56df52e69726d95a400a43bc7747c
Daniil Pavlov <17dp15@queensu.ca>
2024-05-18 13:11:43 -0400

    Added basic temp cart functionality in REST methods and basic UI

--- src/main/java/com/pavlov/onlinestore/controllers/mvc/MainMVCController.java
+++ src/main/java/com/pavlov/onlinestore/controllers/mvc/MainMVCController.java
@@ -1,22 +1,32 @@
 package com.pavlov.onlinestore.controllers.mvc;
 
+import com.pavlov.onlinestore.dao.ProductDAO;
+import com.pavlov.onlinestore.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class MainMVCController {
 
-    @GetMapping(value = {"/", "index"})
-    public String mainPage() {
+    @Autowired
+    ProductDAO productDAO;
 
-        return "index";
+    @GetMapping(value = {"/", "index"})
+    public String mainPage(Model model) {
+        List<Product> all_products = productDAO.allProducts();
+        model.addAttribute("products", all_products);
+        return "products_for_sale";
     }
 
     @GetMapping("/test_design")
     public String mainDesign() {
 
-        return "fragments/main_design";
+        return "main_design";
     }
 
 }
--- src/main/java/com/pavlov/onlinestore/controllers/rest/CartController.java
+++ src/main/java/com/pavlov/onlinestore/controllers/rest/CartController.java
@@ -1,20 +1,30 @@
 package com.pavlov.onlinestore.controllers.rest;
 
+import com.pavlov.onlinestore.dao.ProductDAO;
 import com.pavlov.onlinestore.model.CartLine;
 import com.pavlov.onlinestore.model.Product;
+import jakarta.servlet.http.HttpSession;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/cart")
 @RestController
 public class CartController {
+
+    private static final String HTTP_SESSION_CART_KEY = "session_cart";
+
+    @Autowired
+    ProductDAO productDAO;
 
     @Autowired
     DataSource dataSource;
@@ -47,6 +57,70 @@ public class CartController {
         statement.setInt(3, quantity);
         statement.execute();
         return 0;
+    }
+
+    @SneakyThrows
+    @PostMapping("/temp_cart")
+    public Integer addToTempCart(@RequestParam int product_id, @RequestParam int quantity, HttpSession http_session) {
+
+        Object cart_obj = http_session.getAttribute(HTTP_SESSION_CART_KEY);
+        Map<Integer, Integer> cart;
+        if (cart_obj == null) {
+            cart = new HashMap<>();
+        } else {
+            cart = (Map<Integer, Integer>)cart_obj;
+        }
+
+        cart.put(product_id, quantity);
+        http_session.setAttribute(HTTP_SESSION_CART_KEY, cart);
+
+        return 0;
+    }
+
+    @SneakyThrows
+    @GetMapping("/temp_cart")
+    public List<CartLine> getFromTempCart(HttpSession http_session) {
+
+        List<CartLine> result = new ArrayList<>();
+        Object cart_obj = http_session.getAttribute(HTTP_SESSION_CART_KEY);
+
+        if (cart_obj != null) {
+            Map<Integer, Integer> cart = (Map<Integer, Integer>)cart_obj;
+            for (Integer product_id : cart.keySet()) {
+                if (cart.get(product_id) > 0) {
+                    CartLine cart_line = new CartLine();
+                    cart_line.setQuantity(cart.get(product_id));
+                    cart_line.setProduct_id(product_id);
+                    cart_line.setCart_id(0);
+
+                    Product product = productDAO.getProductById(product_id);
+                    cart_line.setProduct_name(product.getName());
+                    cart_line.setPrice(new BigDecimal(0)); //TODO set price
+
+                    result.add(cart_line);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @SneakyThrows
+    @GetMapping("/temp_cart_lines_quantity")
+    public Integer getLinesQuantity(HttpSession http_session) {
+
+        int result = 0;
+        Object cart_obj = http_session.getAttribute(HTTP_SESSION_CART_KEY);
+        if (cart_obj != null) {
+            Map<Integer, Integer> cart = (Map<Integer, Integer>)cart_obj;
+            for (Integer product_id : cart.keySet()) {
+                if (cart.get(product_id) > 0) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
     }
 
     @SneakyThrows
